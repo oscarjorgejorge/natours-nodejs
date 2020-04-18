bbb49a1468e0c771d567ea0af9e15aa0bf25266a
Oscar Jorge <oscar.jorge.jorge@hotmail.com>
2020-04-18 20:11:06 +0200

    stripe messages added

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -5,6 +5,16 @@ const Bookings = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+exports.alerts = (req, res, next) => {
+    const { alert } = req.query;
+
+    if (alert === 'booking') {
+        res.locals.alert = `Your booking was succesful! Please check your email for a confirmation. 
+        If your booking doesn't show up here immediatly, please come back later.`;
+    }
+    next();
+};
+
 exports.getOverview = catchAsync(async(req, res, next) => {
     // 1) Get tour data from collection
     const tours = await Tour.find();
